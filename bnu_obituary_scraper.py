a826db0df5422734c6b28c02e28eb523766ec9c5
OdilonJK <kroger.odilonj@gmail.com>
2019-02-15 21:34:56 -0200

    Changed source and user agent

--- bnu_obituary_scraper.py
+++ bnu_obituary_scraper.py
@@ -6,32 +6,45 @@ from scrapy.linkextractors import LinkExtractor
 
 class BnuObituaryScraperSpider(CrawlSpider):
     name = 'bnu-obituary-scraper'
-    allowed_domains = ['omunicipioblumenau.com.br']
-    start_urls = ['https://omunicipioblumenau.com.br/']
+
+    custom_settings = {
+        'BOT_NAME': 'bnu_obituary_scraper',
+        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/65.0',
+        'ROBOTSTXT_OBEY': False,
+    }
+
+    allowed_domains = ['arvorespelavida.org.br']
+    start_urls = ['https://www.arvorespelavida.org.br/']
 
     rules = (
         Rule(
             LinkExtractor(
-                restrict_xpaths=('//a[re:test(@href, "obituario")]')
+                restrict_xpaths=('//div/ul[@class="menu"]/li/a[re:test(@href, "obituario")]')
             )
         ),
         Rule(
             LinkExtractor(
-                restrict_xpaths=('//div[@class="td-block-span6"]//div[@class="item-details"]/h3/a')
+                restrict_xpaths=('//div[@class="obituario-inner"]/a')
             ),
             callback='parse_new'
         )
     )
 
     def parse_new(self, response):
-        deceased_people = response.xpath('//div[@class="td-post-content"]/p/span[contains(text(), "sepultad")]/text()').getall()
-        for deceased in deceased_people:
-            deceased_info = deceased.split(',')
-            name = deceased_info[0]
-            age = deceased_info[1].strip()
-            buried_in = deceased_info[2].strip()[deceased_info[2].index('m '):].strip().strip('.')
-            yield {
-                'name': name,
-                'age': age,
-                'buried_in': buried_in
-            }
+        content = response.xpath('//div[@class="content-inner"]')
+
+        name = content.xpath('//p[@class="info-nome"]/text()').get()
+        info = content.xpath('//ul[@class="info-dados"]/li/text()').getall()
+        age = info[0].strip().split(' ')[0]
+        death_date = info[1].strip()
+        burial_date = info[2].strip()
+        # funeral_place =
+        # funeral_coordinates =
+        # burial_place =
+        # burial_coordinates =
+        yield {
+            'name': name,
+            'age': age,
+            'death_date': death_date,
+            'burial_date': burial_date
+        }
